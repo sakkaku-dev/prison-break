cf743360001253ead7f075588f3d76caadb8b779
myin <minyin142p@gmail.com>
2024-02-13 20:01:20 +0100

    update sprites for door inputs

--- godot/DoorInputs.gd
+++ godot/DoorInputs.gd
@@ -1,6 +1,9 @@
 class_name DoorInputs
 extends Control
 
+@export var opened_color := Color.GREEN
+@export var closed_color := Color.RED
+
 @onready var top = $DoorInputs/Top
 @onready var bot = $DoorInputs/Bot
 @onready var left = $DoorInputs/Left
@@ -9,6 +12,7 @@ extends Control
 @onready var hide_pos = Vector2.DOWN * 10
 
 var tw: Tween
+var _doors := {}
 
 func _ready():
 	modulate = Color.TRANSPARENT
@@ -18,12 +22,14 @@ func setup_for_doors(doors: Dictionary):
 	bot.visible = Vector2.DOWN in doors
 	left.visible = Vector2.LEFT in doors
 	right.visible = Vector2.RIGHT in doors
+	_doors = doors
 
 func open():
 	if tw and tw.is_running():
 		tw.kill()
 		
 	position = hide_pos
+	_update_doors()
 	
 	tw = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO).set_parallel()
 	tw.tween_property(self, "modulate", Color.WHITE, 1.0)
@@ -36,3 +42,13 @@ func close():
 	tw = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO).set_parallel()
 	tw.tween_property(self, "modulate", Color.TRANSPARENT, 1.0)
 	tw.tween_property(self, "position", hide_pos, 0.5)
+
+func _update_doors():
+	if top.visible:
+		top.modulate = opened_color if _doors[Vector2.UP].is_open else closed_color
+	if bot.visible:
+		bot.modulate = opened_color if _doors[Vector2.DOWN].is_open else closed_color
+	if right.visible:
+		right.modulate = opened_color if _doors[Vector2.RIGHT].is_open else closed_color
+	if left.visible:
+		left.modulate = opened_color if _doors[Vector2.LEFT].is_open else closed_color
